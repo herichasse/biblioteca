01bd3528fe0ef25e1843edeb3a6bc9052ea26b5a
Hernan <hernanrichasse@gmail.com>
2025-04-02 20:51:36 -0300

    tercer commit libro arreglado

--- Biblioteca/src/main/java/com/example/Biblioteca/model/LibroModel.java
+++ Biblioteca/src/main/java/com/example/Biblioteca/model/LibroModel.java
@@ -1,5 +1,22 @@
 package com.example.Biblioteca.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class LibroModel {
+
+    private int id;
+    private String isbn;
+    private String titulo;
+    private String editorial;
+    private int fechaPublicacion;
+    private String autor;
+
+
 
 }
